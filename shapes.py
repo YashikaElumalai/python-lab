2b33c112125a69c6b403d8985f7a82cc7f0ecb69
YashikaElumalai <yashikaelumalai19@gmail.com>
2024-12-10 14:14:21 +0530

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,19 @@
+print("circle")
+r=int(input("enter the radius:"))
+ans=3.14*(r*r)
+print("area of circle:",ans)
+
+print("rectangle")
+l=int(input("enter the lenght:"))
+b=int(input("enter the breadth:"))
+print("area of rectangle:",l*b)
+
+print("triangle")
+l=int(input("enter the lenght:"))
+b=int(input("enter the breadth:"))
+print("area of triangle:",(1/2)*l*b)
+
+print("square")
+a=int(input("enter the area:"))
+print("area of square:",a*a)
+        
